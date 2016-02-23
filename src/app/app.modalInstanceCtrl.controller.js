c44c74f6f044f69d5248cbc78f652c65d14241c1
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-02-23 17:35:25 +0300

    added for save legacy code

---
+++ src/app/app.modalInstanceCtrl.controller.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function (ngApp) {
+
+    // CONFIG define
+    ngApp.controller('modalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+        $scope.items = items;
+        $scope.selected = {
+            item: $scope.items[0]
+        };
+
+        $scope.ok = function () {
+            $uibModalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    })
+};
