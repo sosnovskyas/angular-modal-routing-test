5e5782c908ec40df490724b1537c03ab66d4a49a
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-02-23 17:33:50 +0300

    remove global controller dependency and require bootstrap.css

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,12 @@
 import angular from 'angular';
+// angular-ui-bootstrap require bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import uiBootstrap from 'angular-ui-bootstrap';
 import uiRouter from 'angular-ui-router';
 
 const ngApp = angular.module('app', [uiRouter, uiBootstrap]);
 
 // including config
-require('./app.config.js')(ngApp);
+require('./app.config')(ngApp);
+//require('./app.modalCtrl.controller')(ngApp);
+//require('./app.modalInstanceCtrl.controller')(ngApp);
